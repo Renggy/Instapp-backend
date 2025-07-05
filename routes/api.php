ebfcab131604255c35d625b26d339fd681268a33
Renggy <renggyn@gmail.com>
2025-07-05 05:57:56 +0000

    Add authentication routes for sign in and sign up

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('auth')
+    ->group(function () {
+        Route::post('/signin', [AuthController::class, 'signIn']);
+        Route::post('/signup', [AuthController::class, 'signUp']);
+    });
