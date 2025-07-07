7f7a00dbdc23449f3113816f3d42a7573e4fed97
Renggy <renggyn@gmail.com>
2025-07-07 06:25:44 +0000

    Enchance coloumn User, eloquent follower and attribute cast

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -17,8 +18,10 @@ class User extends Authenticatable
     protected $fillable = [
         'user_name',
         'user_email',
+        'user_full_name',
         'password',
         'user_avatar',
+        'user_bio'
     ];
 
     protected $hidden = [
@@ -32,5 +35,32 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password'          => 'hashed',
         ];
+    }
+
+    public function post()
+    {
+        return $this->hasMany(Post::class, 'user_id', 'user_id');
+    }
+
+    // User yang saya ikuti
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'follows', 'follower_id', 'following_id')
+            ->withTimestamps();
+    }
+
+    // User yang mengikuti saya
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'follows', 'following_id', 'follower_id')
+            ->withTimestamps();
+    }
+
+    // Atrribute
+    protected function userAvatar() : Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => asset('storage/' . $value),
+        );
     }
 }
