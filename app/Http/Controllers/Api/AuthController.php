328d2b0b0bfe1cd842bf18ccd07a14ebe685352b
Renggy <renggyn@gmail.com>
2025-07-07 06:20:50 +0000

    Update SignUp and Add Signout and Verify user

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -32,10 +32,24 @@ class AuthController extends Controller
     public function signUp(RequestSignUp $request)
     {
         User::create([
-            'user_name'  => $request->user_name,
-            'user_email' => $request->user_email,
-            'password'   => $request->password,
+            'user_name'      => $request->user_name,
+            'user_full_name' => $request->user_full_name,
+            'user_email'     => $request->user_email,
+            'password'       => $request->password,
         ]);
         return ApiResponse::created();
+    }
+
+    public function signOut(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return ApiResponse::success([], "Logged out successfully");
+    }
+
+    public function verify(Request $request)
+    {
+        return ApiResponse::success([
+            'user'  => $request->user()
+        ]);
     }
 }
