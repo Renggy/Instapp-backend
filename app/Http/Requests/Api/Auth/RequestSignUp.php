f21ecabde0af523013895432e79778086b3de3b5
Renggy <renggyn@gmail.com>
2025-07-07 06:22:44 +0000

    Enchance Request For SignUp

--- app/Http/Requests/Api/Auth/RequestSignUp.php
+++ app/Http/Requests/Api/Auth/RequestSignUp.php
@@ -14,10 +14,28 @@ class RequestSignUp extends FormRequest
     public function rules(): array
     {
         return [
-            'user_email' => 'required|email|unique:users,user_email',
-            'user_name'  => 'required|max:255|unique:users,user_name',
-            'password'   => 'required',
-            'confirmPassword' => 'required|same:password'
+            'user_email'      => 'required|email|unique:users,user_email|max:255',
+            'user_full_name'  => 'required|max:255',
+            'password'        => 'required',
+            'confirmPassword' => 'required|same:password',
+            'user_name'       => [
+                'required',
+                'max:255',
+                'unique:users,user_name',
+                'alpha_num',     // hanya huruf dan angka
+                'regex:/^\S*$/u' // tidak boleh ada spasi
+            ],
+        ];
+    }
+
+    public function attributes() : array
+    {
+        return [
+            'user_email'      => 'email address',
+            'user_full_name'  => 'full name',
+            'user_name'       => 'username',
+            'password'        => 'password',
+            'confirmPassword' => 'confirm password',
         ];
     }
 }
